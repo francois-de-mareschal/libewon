5a94f1569dd1e6895e740b9422c1c8d89b5654e5
François de Mareschal <francois.de-mareschal@outlook.fr>
2021-11-17 23:56:17 +0100

    Write a light library documentation to explain its purpose

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,19 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+//! Client for the M2Web REST API.
+//!
+//! The M2Web REST API provides access and control to the eWONs which belong to the user. The client allows
+//! to connect to the M2Web API, to get the details of all the eWONs or one in particular, to get properties
+//! from the eWON or to send it commands.
+//!
+//! The developer documentation for the M2Web REST API could be found
+//! [here](https://developer.ewon.biz/content/m2web-api-0).
+//!
+//! # Example
+//! ```rust
+//! # use libewon::m2web::client::ClientBuilder;
+//! let _client = ClientBuilder::default()
+//!     .t2m_url("https://m2web.talk2m.com/t2mapi")
+//!     .build()
+//!     .unwrap();
+//! ```
+
+pub mod m2web;
