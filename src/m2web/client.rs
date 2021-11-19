a56ee00af48fff07345bef5e239b43224aa1bf2e
François de Mareschal <fdemareschal@sramag.com>
2021-11-19 08:22:40 +0100

    Add unit tests to check Talk2m base url settings

--- src/m2web/client.rs
+++ src/m2web/client.rs
@@ -9,3 +9,26 @@ pub struct Client<'a> {
     #[builder(setter(strip_option), default = "\"https://m2web.talk2m.com/t2mapi\"")]
     t2m_url: &'a str,
 }
+
+#[cfg(test)]
+mod test {
+    use crate::m2web::client;
+
+    #[test]
+    fn build_client_default_t2m_url() -> Result<(), client::ClientBuilderError> {
+        let client = client::ClientBuilder::default().build()?;
+        assert_eq!(client.t2m_url, "https://m2web.talk2m.com/t2mapi");
+
+        Ok(())
+    }
+
+    #[test]
+    fn build_client_custom_t2m_url() -> Result<(), client::ClientBuilderError> {
+        let client = client::ClientBuilder::default()
+            .t2m_url("https://data.talk2m.com")
+            .build()?;
+        assert_eq!(client.t2m_url, "https://data.talk2m.com");
+
+        Ok(())
+    }
+}
