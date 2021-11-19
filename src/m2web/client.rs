3d5bfd2cf2860436cf90983b227be5650e212334
François de Mareschal <fdemareschal@sramag.com>
2021-11-19 16:15:47 +0100

    Add HTTP client instance and test it against a mock server

--- src/m2web/client.rs
+++ src/m2web/client.rs
@@ -1,4 +1,5 @@
 use derive_builder::Builder;
+use reqwest;
 
 /// M2Web API client.
 ///
@@ -8,11 +9,15 @@ pub struct Client<'a> {
     /// The API base url.
     #[builder(setter(strip_option), default = "\"https://m2web.talk2m.com/t2mapi\"")]
     t2m_url: &'a str,
+    /// HTTP client to connect to the API.
+    #[builder(setter(strip_option), default = "reqwest::Client::new()")]
+    http_client: reqwest::Client,
 }
 
 #[cfg(test)]
 mod test {
     use crate::m2web::client;
+    use wiremock::{matchers::method, Mock, MockServer, ResponseTemplate};
 
     #[test]
     fn build_client_default_t2m_url() -> Result<(), client::ClientBuilderError> {
@@ -30,5 +35,27 @@ mod test {
         assert_eq!(client.t2m_url, "https://data.talk2m.com");
 
         Ok(())
+    }
+
+    #[tokio::test]
+    async fn client_connect_to_api_base_url() {
+        let server = MockServer::start().await;
+        let server_uri = &server.uri();
+        let client = client::ClientBuilder::default()
+            .t2m_url(server_uri)
+            .build()
+            .unwrap();
+        Mock::given(method("GET"))
+            .respond_with(ResponseTemplate::new(200))
+            .mount(&server)
+            .await;
+        let status = client
+            .http_client
+            .get(server_uri)
+            .send()
+            .await
+            .unwrap()
+            .status();
+        assert_eq!(status, reqwest::StatusCode::OK);
     }
 }
