5195c1f1d9383288962f2dd884363e53ef80fbbe
François de Mareschal <francois.de-mareschal@outlook.fr>
2021-11-24 00:34:05 +0100

    Add private method to build complete API URL with auth parameters

--- src/m2web/client.rs
+++ src/m2web/client.rs
@@ -28,7 +28,7 @@ pub struct Client<'a> {
 
 impl<'a> Client<'a> {
     /// Build the authentication parameters to be passed in the url.
-    fn stateless_auth_parameters(&self) -> String {
+    fn build_url_stateless_auth_params(&self) -> String {
         let mut parameters = String::new();
 
         parameters.push_str(format!("t2maccount={}", self.t2m_account).as_str());
@@ -41,12 +41,24 @@ impl<'a> Client<'a> {
 
         parameters
     }
+
+    /// Build the URL with base, parameters and endpoint.
+    fn build_url(&self) -> String {
+        let mut request_url = String::new();
+
+        request_url.push_str(self.t2m_url);
+        // TODO: build endpoint.
+        request_url.push('?');
+        request_url.push_str(self.build_url_stateless_auth_params().as_str());
+
+        request_url
+    }
 }
 
 #[cfg(test)]
 mod test {
     use crate::m2web::client;
-    use wiremock::{matchers::method, Mock, MockServer, ResponseTemplate};
+    use wiremock::{matchers::method, matchers::query_param, Mock, MockServer, ResponseTemplate};
 
     #[test]
     fn build_client_default_t2m_url() -> Result<(), client::ClientBuilderError> {
@@ -169,7 +181,7 @@ mod test {
         let generated_auth_params = "t2maccount=account1&t2musername=username1&t2mpassword=password1&t2mdeveloperid=731e38ec-981f-4f31-9cb5-e87f0d571816";
         let client = client::ClientBuilder::default().build().unwrap();
 
-        let auth_params = client.stateless_auth_parameters();
+        let auth_params = client.build_url_stateless_auth_params();
 
         assert_eq!(generated_auth_params, auth_params.as_str());
     }
@@ -185,8 +197,62 @@ mod test {
             .build()
             .unwrap();
 
-        let auth_params = client.stateless_auth_parameters();
+        let auth_params = client.build_url_stateless_auth_params();
 
-        assert_eq!(generated_auth_params, auth_params.as_str());
+        assert_eq!(generated_auth_params, auth_params);
+    }
+
+    #[test]
+    fn build_url_default_params_no_endpoint() {
+        let generated_url = "https://m2web.talk2m.com/t2mapi?t2maccount=account1&t2musername=username1&t2mpassword=password1&t2mdeveloperid=731e38ec-981f-4f31-9cb5-e87f0d571816";
+        let client = client::ClientBuilder::default().build().unwrap();
+
+        let url = client.build_url();
+
+        assert_eq!(generated_url, url);
+    }
+
+    #[test]
+    fn build_url_custom_params_no_endpoint() {
+        let generated_url = "https://m2web.talk2m.com/t2mapi?t2maccount=account2&t2musername=username2&t2mpassword=password2&t2mdeveloperid=795f1844-2f5e-4d8b-9922-25c45d3e1c47";
+        let client = client::ClientBuilder::default()
+            .t2m_account("account2")
+            .t2m_username("username2")
+            .t2m_password("password2")
+            .t2m_developer_id("795f1844-2f5e-4d8b-9922-25c45d3e1c47")
+            .build()
+            .unwrap();
+
+        let url = client.build_url();
+
+        assert_eq!(generated_url, url);
+    }
+
+    #[tokio::test]
+    async fn stateless_auth_default_params_ko() -> Result<(), reqwest::Error> {
+        let server = MockServer::start().await;
+        let server_uri = &server.uri();
+
+        let client = client::ClientBuilder::default()
+            .t2m_url(server_uri)
+            .build()
+            .unwrap();
+        Mock::given(method("GET"))
+            .and(query_param("t2maccount", "account1"))
+            .and(query_param("t2musername", "username1"))
+            .and(query_param("t2mpassword", "password1"))
+            .and(query_param(
+                "t2mdeveloperid",
+                "731e38ec-981f-4f31-9cb5-e87f0d571816",
+            ))
+            .respond_with(ResponseTemplate::new(200))
+            .mount(&server)
+            .await;
+        let url = client.build_url();
+        let status = client.http_client.get(url).send().await?.status();
+
+        assert_eq!(status, reqwest::StatusCode::OK);
+
+        Ok(())
     }
 }
