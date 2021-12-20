3c0562289e9c71c29c14546ff4fd1f29d0e1fccb
François de Mareschal <fdemareschal@sramag.com>
2021-12-20 19:15:51 +0100

    Test logout() in normal conditions

--- src/m2web/client.rs
+++ src/m2web/client.rs
@@ -169,7 +169,7 @@ impl<'a> Client<'a> {
                 _ => {
                     if let Some(ref t2m_session) = self.t2m_session {
                         vec![
-                            ("t2msessionid", t2m_session.as_ref()),
+                            ("t2msession", t2m_session.as_ref()),
                             ("t2mdeveloperid", self.t2m_developer_id),
                         ]
                     } else {
@@ -268,6 +268,7 @@ mod test {
             .and(query_param("pool", ""))
             .and(path("/t2mapi/getewons"))
             .respond_with(ResponseTemplate::new(200).set_body_json(&json_response))
+            .expect(1)
             .mount(&server)
             .await;
 
@@ -347,6 +348,7 @@ mod test {
             .and(query_param("pool", ""))
             .and(path("/t2mapi/getewons"))
             .respond_with(ResponseTemplate::new(200).set_body_json(&json_response))
+            .expect(1)
             .mount(&server)
             .await;
 
@@ -435,6 +437,7 @@ mod test {
             .and(query_param("pool", ""))
             .and(path("/t2mapi/getewons"))
             .respond_with(ResponseTemplate::new(200).set_body_json(&json_response))
+            .expect(1)
             .mount(&server)
             .await;
 
@@ -482,6 +485,7 @@ mod test {
             ))
             .and(path("/t2mapi/login"))
             .respond_with(ResponseTemplate::new(403).set_body_json(&json_response))
+            .expect(1)
             .mount(&server)
             .await;
 
@@ -532,6 +536,7 @@ mod test {
             ))
             .and(path("/t2mapi/login"))
             .respond_with(ResponseTemplate::new(200).set_body_json(&json_response))
+            .expect(1)
             .mount(&server)
             .await;
 
@@ -567,6 +572,7 @@ mod test {
             ))
             .and(path("/t2mapi/login"))
             .respond_with(ResponseTemplate::new(403).set_body_json(&json_response))
+            .expect(1)
             .mount(&server)
             .await;
 
@@ -590,7 +596,62 @@ mod test {
 
     #[tokio::test]
     async fn config_stateful_logout_ok() -> Result<(), error::Error> {
-        todo!()
+        let server = MockServer::start().await;
+        let server_uri = format!("{}/t2mapi", &server.uri());
+        let mut client = client::ClientBuilder::default()
+            .t2m_url(&server_uri)
+            .t2m_account("account2")
+            .t2m_username("username2")
+            .t2m_password("password2")
+            .t2m_developer_id("795f1844-2f5e-4d8b-9922-25c45d3e1c47")
+            .stateful_auth(true)
+            .build()
+            .unwrap();
+
+        let json_response_login = json!({
+          "t2msession": "e44be62aaa9381707b5ab328c18d4a43",
+          "success": true
+        });
+
+        let json_response_logout = json!({
+            "success": true
+        });
+
+        Mock::given(method("GET"))
+            .and(query_param("t2maccount", "account2"))
+            .and(query_param("t2musername", "username2"))
+            .and(query_param("t2mpassword", "password2"))
+            .and(query_param(
+                "t2mdeveloperid",
+                "795f1844-2f5e-4d8b-9922-25c45d3e1c47",
+            ))
+            .and(path("/t2mapi/login"))
+            .respond_with(ResponseTemplate::new(200).set_body_json(&json_response_login))
+            .expect(1)
+            .named("login")
+            .mount(&server)
+            .await;
+
+        Mock::given(method("GET"))
+            .and(query_param(
+                "t2msession",
+                "e44be62aaa9381707b5ab328c18d4a43",
+            ))
+            .and(query_param(
+                "t2mdeveloperid",
+                "795f1844-2f5e-4d8b-9922-25c45d3e1c47",
+            ))
+            .and(path("/t2mapi/logout"))
+            .respond_with(ResponseTemplate::new(200).set_body_json(&json_response_logout))
+            .expect(1)
+            .named("logout")
+            .mount(&server)
+            .await;
+
+        let _ = client.login().await?;
+        client.logout().await?;
+
+        Ok(())
     }
 
     #[tokio::test]
