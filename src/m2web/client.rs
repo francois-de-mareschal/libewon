e529f317987092d7e449257319232ee760a0759a
François de Mareschal <fdemareschal@sramag.com>
2021-12-21 10:58:32 +0100

    Test if logout fails when the session id is missing

--- src/m2web/client.rs
+++ src/m2web/client.rs
@@ -729,7 +729,73 @@ mod test {
 
     #[tokio::test]
     async fn config_stateful_logout_ko_no_session_id() -> Result<(), error::Error> {
-        todo!()
+        let server = MockServer::start().await;
+        let server_uri = format!("{}/t2mapi", &server.uri());
+        let mut client = client::ClientBuilder::default()
+            .t2m_url(&server_uri)
+            .t2m_account("account2")
+            .t2m_username("username2")
+            .t2m_password("password2")
+            .t2m_developer_id("795f1844-2f5e-4d8b-9922-25c45d3e1c47")
+            .stateful_auth(true)
+            .build()
+            .unwrap();
+
+        let json_response_login = json!({
+          "t2msession": "e44be62aaa9381707b5ab328c18d4a43",
+          "success": true
+        });
+
+        let json_response_logout = json!({
+          "success": true
+        });
+
+        Mock::given(method("GET"))
+            .and(query_param("t2maccount", "account2"))
+            .and(query_param("t2musername", "username2"))
+            .and(query_param("t2mpassword", "password2"))
+            .and(query_param(
+                "t2mdeveloperid",
+                "795f1844-2f5e-4d8b-9922-25c45d3e1c47",
+            ))
+            .and(path("/t2mapi/login"))
+            .respond_with(ResponseTemplate::new(200).set_body_json(&json_response_login))
+            .expect(1)
+            .named("login")
+            .mount(&server)
+            .await;
+
+        Mock::given(method("GET"))
+            .and(query_param(
+                "t2msession",
+                "e44be62aaa9381707b5ab328c18d4a43",
+            ))
+            .and(query_param(
+                "t2mdeveloperid",
+                "795f1844-2f5e-4d8b-9922-25c45d3e1c47",
+            ))
+            .and(path("/t2mapi/logout"))
+            .respond_with(ResponseTemplate::new(200).set_body_json(&json_response_logout))
+            .expect(0)
+            .named("logout")
+            .mount(&server)
+            .await;
+
+        let _ = client.login().await?;
+        client.t2m_session = None;
+        let logout = client.logout().await;
+
+        assert_eq!(
+            logout,
+            Err(error::Error {
+                code: 403,
+                kind: error::ErrorKind::InvalidCredentials(
+                    "No session opened, please login before requesting the API".to_string(),
+                ),
+            })
+        );
+
+        Ok(())
     }
 
     #[tokio::test]
